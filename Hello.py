70d151d659bc6dd049209298d6feef34c85445c6
ferreali <alini.ferre@gmail.com>
2025-03-21 08:32:27 -0300

    atividade 1
    primeiro exercicio usando python

---
+++ Hello.py
@@ -0,0 +1,6 @@
+nome = input("Digite seu nome:") #entrada de informacao do usuario,variavel é um apelido que eh armazenado na memoria
+print("Boa noite,",nome) #saida da informacao na tela
+idade = input("Digite sua idade:")
+print("vc tem ", idade, "anos")
+
+#input recebe dados de entrada
