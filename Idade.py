3d11a8432d4c537768f6defb47fc4ae243d631e9
ferreali <alini.ferre@gmail.com>
2025-03-21 08:33:21 -0300

    exercicio 2
    atividade usando os primeiros comandos

---
+++ Idade.py
@@ -0,0 +1,12 @@
+idade = int (input("Qual a sua idade? "))
+
+if (idade >= 18): #condicao verdadeira true
+        print("Maior de idade.Venda liberada")#4 espacos
+elif (idade >=12):#mais de uma condicao alem do if e else,eh elif
+     print("Adolescente. Somente guaraná")        
+else: #condicao falsa
+        print("Menor de idade.Venda proibida") #4 espacos
+
+print("Fim da venda")#sem espacamento
+
+
