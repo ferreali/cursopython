190d03e376ecc353d5cc7fa7d6f6e32a0763014d
ferreali <alini.ferre@gmail.com>
2025-03-21 08:36:53 -0300

    exercicio 5
    novos procedimentos para realizar soma,uso de condicoes e variaveis

---
+++ soma.py
@@ -0,0 +1,10 @@
+#entrada de dados
+primeiroNumero = float (input("digite 1 numero: ")) #float numeros flutuante
+segundoNumero  = float (input("digite o outro numero: "))
+#processamento
+resultado = primeiroNumero + segundoNumero #convertendo string para numero inteiro,passar int na frente da variavel
+
+
+#saida de dados
+print ("A soma dos numeros eh: ", resultado)
+
